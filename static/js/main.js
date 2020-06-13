0c92db9f0690427c24566da3d689f387c2d4aa54
SHIVA4042000 <60439570+SHIVA4042000@users.noreply.github.com>
2020-06-13 18:57:47 +0530

    final commit

--- static/js/main.js
+++ static/js/main.js
@@ -66,5 +66,5 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}    
 
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
